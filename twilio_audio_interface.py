33f51c7e0c4948cc9d068180b556abc03c42441b
Anchit Saluja <67328749+hairaat@users.noreply.github.com>
2025-06-01 23:53:07 +0530

    Add start and stop methods to TwilioAudioInterface for ElevenLabs SDK

--- twilio_audio_interface.py
+++ twilio_audio_interface.py
@@ -8,18 +8,13 @@ class TwilioAudioInterface:
         self.audio_queue = asyncio.Queue()
         self.closed = False
 
-    # Called by ElevenLabs to get user audio
     async def get_audio(self):
-        # Wait for Twilio to provide audio via push_user_audio
         return await self.audio_queue.get()
 
-    # Called by your Twilio handler for every 'media' event from Twilio
     async def push_user_audio(self, audio_bytes):
         await self.audio_queue.put(audio_bytes)
 
-    # Called by ElevenLabs to play agent audio
     async def play_agent_audio(self, audio_bytes):
-        # Immediately send to Twilio in the correct format
         payload = base64.b64encode(audio_bytes).decode()
         await self.websocket.send_text(json.dumps({
             "event": "media",
@@ -28,4 +23,11 @@ class TwilioAudioInterface:
 
     async def close(self):
         self.closed = True
+
+    # Add these two methods:
+    def start(self, *args, **kwargs):
+        pass
+
+    def stop(self, *args, **kwargs):
+        pass
 
